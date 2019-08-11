47fcc1138de59ce91bce28e3ab66594c0bd4f0ea
vibhu <53313241+vibhu-hada@users.noreply.github.com>
2019-08-11 13:55:54 +0530

    Add files via upload

---
+++ q1.sh
@@ -0,0 +1,5 @@
+echo "Enter your name."
+read nm
+mkdir $nm
+touch $nm/$nm.cpp
+
---
+++ q2.sh
@@ -0,0 +1,4 @@
+echo "Enter your name."
+read nm
+mkdir $nm
+touch $nm/$nm"1".cpp $nm/$nm"2".cpp $nm/$nm"3".cpp $nm/$nm"4".cpp $nm/$nm"5".cpp
